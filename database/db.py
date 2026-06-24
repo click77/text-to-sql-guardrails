1a58450856a71c76af7247889041e0e73203a5f2
click77 <alexyong77@hotmail.com>
2026-06-24 16:07:53 +0800

    Setup PostgreSQL and SQLAlchemy connection

--- database/db.py
+++ database/db.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/text2sql"
 
 engine = create_engine(
     DATABASE_URL,
